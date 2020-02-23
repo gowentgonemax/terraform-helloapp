729271bb1748072e795faf946f271114fd25e020
Ravi shah <44101788+iamshahh@users.noreply.github.com>
2020-02-23 14:00:44 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_launch_configuration" "default" {
   name          = "terraform-springboot-helloapp"
   image_id      = "ami-04ff587bc1c1d65e6"
   instance_type = "t2.micro"
-  key_name = "springboot-rest-mysql-ravi"
+  key_name = "mykey"
   security_groups = ["${aws_security_group.default.id}"]
   iam_instance_profile = "${aws_iam_instance_profile.default.name}"
   user_data = "${data.template_file.default.rendered}"
